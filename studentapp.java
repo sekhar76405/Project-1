3eb381da27b18b23563c721bf79d5ddd0574d0c4
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-04-18 17:55:02 -0700

    created the basic outline of the project with sifficient ammount of comments

---
+++ studentapp.java
@@ -0,0 +1,35 @@
+
+public class studentapp {
+	public class student {
+		private String firstname;
+		private String lastname;
+		private String year;
+		private int StudentID;
+		private String courses;
+		private String tutionbalance;
+		private int costofcopurse = 10000;
+
+		// DCN , OOPS, SEPM, Advance Linux and BRM are the subjects. Each costs 10,000
+		// Rs
+
+		// Constructor: prompts user to enter student name and year
+
+		// Generate an ID
+
+		// enroll into courses
+
+		// view balance
+
+		// Pay tuition fees
+
+		// show status
+	}
+
+	public static void main(String[] args) {
+		// Ask how many users do you want to add
+
+		// create n number of new students
+
+	}
+}
+
